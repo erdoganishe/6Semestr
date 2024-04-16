32ad1d331a293247be0836dcb6c068c033ed4151
Erdogan <khotyanmisha@gmail.com>
2024-04-16 11:19:28 +0300

    A lot added

--- ВМПтФ/LB1/lb1/lb1/settings.py
+++ ВМПтФ/LB1/lb1/lb1/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-q0y566mbe-_dnv!hglfi8ve*h#-rzpovboe2eq6ie%imr5_3mu'
+SECRET_KEY = 'django-insecure-8kb=!1oaohxy4=ot&b8m%8h(du%kor9$mo_%w=79r=f%l-gcuf'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    "lb1_app"
 ]
 
 MIDDLEWARE = [
---
+++ ВМПтФ/LB1/lb1/lb1_app/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class Lb1AppConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'lb1_app'
